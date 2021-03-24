b6614766ba26c733db8ea4bb78e92ec6574f6647
SenuraJayadeva <senurajayadeva@gmail.com>
2021-03-24 19:36:45 +0530

    Add project files.

---
+++ DemoProject/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace DemoProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            this.StudentID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(130, 57);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(126, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(130, 100);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(126, 22);
+            this.txtGender.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gender";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(130, 150);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(126, 22);
+            this.txtAge.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Age";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(28, 233);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(130, 233);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(231, 233);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StudentID,
+            this.Name,
+            this.Gender,
+            this.Age});
+            this.dgvStudent.Location = new System.Drawing.Point(327, 42);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.ReadOnly = true;
+            this.dgvStudent.RowHeadersWidth = 51;
+            this.dgvStudent.RowTemplate.Height = 24;
+            this.dgvStudent.Size = new System.Drawing.Size(461, 396);
+            this.dgvStudent.TabIndex = 9;
+            this.dgvStudent.DoubleClick += new System.EventHandler(this.dgvStudent_DoubleClick);
+            // 
+            // StudentID
+            // 
+            this.StudentID.DataPropertyName = "StudentID";
+            this.StudentID.HeaderText = "StudentID";
+            this.StudentID.MinimumWidth = 6;
+            this.StudentID.Name = "StudentID";
+            this.StudentID.ReadOnly = true;
+            this.StudentID.Visible = false;
+            this.StudentID.Width = 125;
+            // 
+            // Name
+            // 
+            this.Name.DataPropertyName = "Name";
+            this.Name.HeaderText = "Name";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            this.Name.Width = 125;
+            // 
+            // Gender
+            // 
+            this.Gender.DataPropertyName = "Gender";
+            this.Gender.HeaderText = "Gender";
+            this.Gender.MinimumWidth = 6;
+            this.Gender.Name = "Gender";
+            this.Gender.ReadOnly = true;
+            this.Gender.Width = 125;
+            // 
+            // Age
+            // 
+            this.Age.DataPropertyName = "Age";
+            this.Age.HeaderText = "Age";
+            this.Age.MinimumWidth = 6;
+            this.Age.Name = "Age";
+            this.Age.ReadOnly = true;
+            this.Age.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvStudent);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+        
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvStudent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+    }
+}
+
---
+++ DemoProject/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoProject
+{
+    public partial class Form1 : Form
+    {
+        StudentTable stu = new StudentTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            txtName.Text = txtGender.Text = txtAge.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            stu.StudentID = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            stu.Name = txtName.Text.Trim();
+            stu.Gender = txtGender.Text.Trim();
+            stu.Age = txtAge.Text.Trim();
+            using (DBEntities db = new DBEntities())
+            {
+                if (stu.StudentID == 0)//INSERT 
+                    db.StudentTables.Add(stu);
+                else //UPDATE
+                    db.Entry(stu).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Submitted Successully");
+        }
+
+
+        void PopulateDataGridView()
+        {
+            dgvStudent.AutoGenerateColumns = false;
+            using (DBEntities db = new DBEntities())
+            {
+                dgvStudent.DataSource = db.StudentTables.ToList<StudentTable>();
+            }
+        }
+
+        private void dgvStudent_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvStudent.CurrentRow.Index != -1)
+            {
+                stu.StudentID = Convert.ToInt32(dgvStudent.CurrentRow.Cells["StudentID"].Value);
+
+                using(DBEntities db =new DBEntities())
+                {
+                    stu = db.StudentTables.Where(x => x.StudentID == stu.StudentID).FirstOrDefault();
+                    txtName.Text = stu.Name;
+                    txtGender.Text = stu.Gender;
+                    txtAge.Text = stu.Age;
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+
+            }
+        }
+    }
+}
